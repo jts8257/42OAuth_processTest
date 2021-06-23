a8bd0d67e72b587e55066a04cf93bab4665afb73
정태성 <tjeong@jeongtaeseong-ui-MacBookAir.local>
2021-06-23 12:52:11 +0900

    gee

--- src/main/java/com/example/_42oauth/controller/MyController.java
+++ src/main/java/com/example/_42oauth/controller/MyController.java
@@ -1,2 +1,27 @@
-package com.example._42oauth.controller;public class MyController {
+package com.example._42oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String now() {
+        return "Receiver";
+    }
+
+    @RequestMapping("/location")
+    public String nw() {
+        return "location";
+    }
+
+    @GetMapping("/main")
+    public String nn() {
+        return "main";
+    }
 }
+
